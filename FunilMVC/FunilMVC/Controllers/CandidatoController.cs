47d30d3fd9b2d1a37b5152ec4623b3a8dd6b22c6
SkeegWyndson <wyndsonb@gmail.com>
2022-08-23 16:53:57 -0300

    FunilMVC

---
+++ FunilMVC/FunilMVC/Controllers/CandidatoController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FunilMVC.Models;
+
+namespace FunilMVC.Controllers
+{
+    public class CandidatoController : Controller
+    {
+        BDFunilEntities bd = new BDFunilEntities();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(bd.CANDIDATO.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CANDIDATO candidato = bd.CANDIDATO.Find(id);
+            if (candidato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(candidato);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string nome, string formacao)
+        {
+            CANDIDATO criar = new CANDIDATO();
+
+            criar.CANNOME = nome;
+            criar.CANFORMACAO = formacao;
+
+            bd.CANDIDATO.Add(criar);
+            bd.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            CANDIDATO can = bd.CANDIDATO.ToList().Where(x => x.CANID == id).First();
+            return View(can);
+        }
+
+        [HttpPost]
+        public ActionResult ConfirmDelete(int? id)
+        {
+            CANDIDATO can = bd.CANDIDATO.ToList().Where(x => x.CANID == id).First();
+
+            bd.CANDIDATO.Remove(can);
+
+            try
+            {
+                bd.SaveChanges();
+            }
+            catch
+            {
+                return RedirectToAction("Erro", "Home");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CANDIDATO can = bd.CANDIDATO.Find(id);
+            if (can == null)
+            {
+                return HttpNotFound();
+            }
+            return View(can);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int? id, string nome, string formacao)
+        {
+            CANDIDATO can = bd.CANDIDATO.ToList().Where(x => x.CANID == id).First();
+
+            can.CANNOME = nome;
+            can.CANFORMACAO = formacao;
+
+            bd.Entry(can).State = System.Data.EntityState.Modified;
+
+            try
+            {
+                bd.SaveChanges();
+            }
+            catch
+            {
+                return RedirectToAction("Erro", "Home");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ FunilMVC/FunilMVC/Controllers/VagaController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FunilMVC.Models;
+
+namespace FunilMVC.Controllers
+{
+    public class VagaController : Controller
+    {
+        BDFunilEntities bd = new BDFunilEntities();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View(bd.VAGA.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VAGA vagas = bd.VAGA.Find(id);
+            if (vagas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vagas);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(string nome, string descricao, string atribuicao, double salario, string requisito, DateTime data)
+        {
+            VAGA vagaCriar = new VAGA();
+
+            vagaCriar.VAINOME = nome;
+            vagaCriar.VAIDESCRICAO = descricao;
+            vagaCriar.VAIATRIBUICAO = atribuicao;
+            vagaCriar.VAISALARIO = Convert.ToDecimal(salario);
+            vagaCriar.VAIREQUISITO = requisito;
+            vagaCriar.VAIDATACADASTRO = data;
+
+            bd.VAGA.Add(vagaCriar);
+            bd.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            VAGA vaga = bd.VAGA.ToList().Where(x => x.VAIID == id).First();
+            return View(vaga);
+        }
+
+        [HttpPost]
+        public ActionResult ConfirmDelete(int? id)
+        {
+            VAGA vagaDeletar = bd.VAGA.ToList().Where(x => x.VAIID == id).First();
+
+            bd.VAGA.Remove(vagaDeletar);
+
+            try
+            {
+                bd.SaveChanges();
+            }
+            catch
+            {
+                return RedirectToAction("Erro", "Home");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            VAGA vagas = bd.VAGA.Find(id);
+            if (vagas == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vagas);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(int? id, string nome, string descricao, string atribuicao, double salario, string requisito, DateTime data)
+        {
+            VAGA vagaEdit = bd.VAGA.ToList().Where(x => x.VAIID == id).First();
+
+            vagaEdit.VAINOME = nome;
+            vagaEdit.VAIDESCRICAO = descricao;
+            vagaEdit.VAIATRIBUICAO = atribuicao;
+            vagaEdit.VAISALARIO = Convert.ToDecimal(salario);
+            vagaEdit.VAIREQUISITO = requisito;
+            vagaEdit.VAIDATACADASTRO = data;
+
+            bd.Entry(vagaEdit).State = System.Data.EntityState.Modified;
+
+            try
+            {
+                bd.SaveChanges();
+            }
+            catch
+            {
+                return RedirectToAction("Erro", "Home");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
